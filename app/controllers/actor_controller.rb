b7ca97c102fece0ba01062147c60d613a468b3b1
pmezamoc <pmezamoc@chicagobooth.edu>
2023-02-02 21:52:27 +0000

    almost

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -7,7 +7,7 @@ end
 
 def actor_details 
   actor_id = params.fetch("actor_id")
-  @filmography = Movie.where({:actor_id => actor_id})
+  @characters = Character.where({:actor_id => actor_id })
   @actor = Actor.where({:id => actor_id}).first
   render({:template => "other_templates/actor_details"})
 end
