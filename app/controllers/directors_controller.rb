e8700d63de076dedff0a0bd42f9c835833ae7327
pmezamoc <pmezamoc@chicagobooth.edu>
2023-02-02 18:10:52 +0000

    Directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,8 +1,13 @@
 class DirectorsController < ApplicationController
   def wisest
+    @oldest =  Director.order({:dob => :asc}).where.not({:dob => nil}).at(0)
     render({:template => "director_templates/eldest.html.erb"})
   end
 
+  def foolest
+    @youngest =  Director.order({:dob => :desc}).where.not({:dob => nil}).at(0)
+    render({:template => "director_templates/youngest.html.erb"})
+  end
 
 
   def index
@@ -10,4 +15,11 @@ class DirectorsController < ApplicationController
   
     render({:template => "director_templates/index.html.erb"})
   end
+
+  def director_details
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({:id => the_id}).first
+    render({:template => "director_templates/show.html.erb"})
+  end
+
 end
